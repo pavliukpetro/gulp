336041ae3f1590a22d68c5b0db19d93e3c514ae3
Peter Pavliuk <hartryfock@gmail.com>
2025-05-22 20:24:37 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { task, src, dest, watch, series } = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+
+const PATH = {
+    html: {
+        src: 'src/*.html',
+        dist: 'dist',
+    },
+    sass: {
+        src: 'src/sass/*.scss',
+        dist: 'dist/css',
+    },
+    js: {
+        src: 'src/js/*.js',
+        dist: 'dist/js',
+    },
+};
+
+task('js', () => {
+    return src(PATH.js.src)
+        .pipe(babel({ presets: ['@babel/env'] }))
+        .pipe(dest(PATH.js.dist));
+});
+
+task('sass', () => {
+    return src(PATH.sass.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
+        .pipe(
+            rename(function (path) {
+                path.basename += '.min';
+            })
+        )
+        .pipe(sourcemaps.write('./maps'))
+
+        .pipe(dest(PATH.sass.dist));
+});
+
+task('html', () => {
+    return src(PATH.html.src).pipe(dest(PATH.html.dist));
+});
+
+task('watch', () => {
+    watch(PATH.html.src, series('html'));
+    watch(PATH.sass.src, series('sass'));
+    watch(PATH.js.src, series('js'));
+});
+
+task('default', series('html', 'sass', 'js'));
